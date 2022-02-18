72dfbe87b5037f31e7bbeee6080446903177cb0f
jyh4358 <jyh4358@gmail.com>
2022-02-18 17:10:49 +0900

    파일 업로드와 다운로드 구현

---
+++ uploadex/src/test/java/hello/uploadex/UploadexApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.uploadex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
