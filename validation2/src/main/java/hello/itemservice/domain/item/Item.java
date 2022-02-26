788804ad52e970b2077e1fef4cfe255cf2f3aad8
jyh4358 <jyh4358@gmail.com>
2022-02-26 20:05:28 +0900

    Bean Validation

---
+++ validation2/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,33 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class Item {
+
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotBlank
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    @NotBlank
+    @Max(9999)
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
